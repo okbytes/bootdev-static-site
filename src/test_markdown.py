666b0ba51c310fde9eecc59f8270d71bdcdf8b8f
Brett Flora <1brettf@gmail.com>
2026-03-19 08:56:06 -0400

    saving progress (if that)

---
+++ src/test_markdown.py
@@ -0,0 +1,58 @@
+import unittest
+
+from src.textnode import TextNode, TextType
+from src.utils import split_nodes_delimiter
+
+
+class TestHTMLNode(unittest.TestCase):
+    def test_simple_code(self):
+        node = TextNode("A word `code block` more word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("A word ", TextType.TEXT, None),
+                TextNode("code block", TextType.CODE, None),
+                TextNode(" more word", TextType.TEXT, None),
+            ]
+        )
+
+    def test_simple_bold(self):
+        node = TextNode("A word **bold block** more word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("A word ", TextType.TEXT, None),
+                TextNode("bold block", TextType.BOLD, None),
+                TextNode(" more word", TextType.TEXT, None),
+            ]
+        )
+
+    def test_simple_italic(self):
+        node = TextNode("A word _italic block_ more word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("A word ", TextType.TEXT, None),
+                TextNode("italic block", TextType.ITALIC, None),
+                TextNode(" more word", TextType.TEXT, None),
+            ]
+        )
+
+
+    def test_tricky_bold(self):
+        node = TextNode("**Bold block** more word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("", TextType.TEXT, None),
+                TextNode("Bold block", TextType.BOLD, None),
+                TextNode(" more word", TextType.TEXT, None),
+            ]
+        )
+
+if __name__ == "__main__":
+    unittest.main()
--- src/utils.py
+++ src/utils.py
@@ -1,2 +1,25 @@
+from src.textnode import TextNode, TextType
+
+
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            nodes.append(old_node)
+            continue
+
+        split = old_node.text.split(delimiter)
+        if len(split) % 2 == 0:
+            raise Exception(f"missing closing delimiter: {delimiter}")
+        for i in range(len(split)):
+            text = split[i]
+            if i % 2 == 0:
+                nodes.append(TextNode(text, TextType.TEXT))
+            else:
+                nodes.append(TextNode(text, text_type))
+
+    return nodes
+
+
+def extract_markdown_images():
     pass
