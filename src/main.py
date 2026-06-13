d927100b4ba7fe745c7510dcd2a1eb401f9efcca
Brett Flora <1brettf@gmail.com>
2026-06-13 19:48:07 -0400

    recursive copy_content

--- src/main.py
+++ src/main.py
@@ -1,9 +1,28 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+
+
+def copy_content(src, dst):
+    if not os.path.exists(src) or not os.path.isdir(src):
+        return
+
+    if not os.path.exists(dst):
+        os.mkdir(dst)
+
+    for path in os.listdir(src):
+        new_src = os.path.join(src, path)
+        new_dst = os.path.join(dst, path)
+
+        if os.path.isdir(new_src):
+            copy_content(new_src, new_dst)
+        elif os.path.isfile(new_src):
+            shutil.copy(new_src, new_dst)
 
 
 def main():
-    node = TextNode("dummy text", TextType.TEXT)
-    print(node)
+    if os.path.exists("./public"):
+        shutil.rmtree("./public")
+    copy_content("./static", "./public")
 
 
 main()
